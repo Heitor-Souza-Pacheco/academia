d7d9fd70b35f6c17af349e895a0a008c6160db76
Heitor-Souza-Pacheco <heitorpacheco2009@gmail.com>
2026-07-03 17:31:13 -0300

    corrigindolazy loading e a chave do Groc

--- src/main/java/com/example/academia/Repositories/FichaRepository.java
+++ src/main/java/com/example/academia/Repositories/FichaRepository.java
@@ -1,12 +1,24 @@
 package com.example.academia.Repositories;
 
 import com.example.academia.Entities.Ficha;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FichaRepository extends JpaRepository<Ficha, Long> {
 
+    @EntityGraph(attributePaths = {"exercicios", "exercicios.video"})
+    List<Ficha> findAll();
+
+    @EntityGraph(attributePaths = {"exercicios", "exercicios.video"})
+    Optional<Ficha> findById(Long id);
+
+    @EntityGraph(attributePaths = {"exercicios", "exercicios.video"})
     List<Ficha> findByCategoria(String categoria);
+
+    @EntityGraph(attributePaths = {"exercicios", "exercicios.video"})
     List<Ficha> findByTituloContainingIgnoreCase(String titulo);
 }
